e08efadb002cafc79713c7f842527b8325fe5555
anderzzz <ohrn.anders@gmail.com>
2020-09-17 14:36:21 +0200

    Expanding the model further. Training feature complete. Bugs and tweaks pending

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 '''
 import torch
 from torch import optim
+import torch.nn.functional as F
 from torch_geometric.data import DataLoader
 
 from rna_dataset import RNADataset
@@ -27,7 +28,7 @@ def rudimentary_test():
 
 def trainer(n_epochs, data_root, batch_size,
             n_weight_versions, n_node_versions,
-            conv_type, conv_kwargs,
+            conv_type, conv_n_iter, conv_kwargs,
             lr_init, scheduler_step_size, scheduler_gamma):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -40,7 +41,7 @@ def trainer(n_epochs, data_root, batch_size,
                        n_latent_edges=n_weight_versions,
                        n_latent_nodes=n_node_versions,
                        n_out_node_props=5,
-                       conv_type=conv_type, conv_kwargs=conv_kwargs)
+                       conv_type=conv_type, conv_n_iter=conv_n_iter, conv_kwargs=conv_kwargs)
 
     optimizer = optim.SGD(lgcn.parameters(), lr=lr_init, momentum=0.9)
     exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer,
@@ -50,7 +51,9 @@ def trainer(n_epochs, data_root, batch_size,
     lgcn.train()
     for k_epoch in range(n_epochs):
 
+        running_loss = 0.0
         for k_batch, data_batch in enumerate(rna_structure_loader):
+            #print (k_batch, data_batch.x.shape)
             data_batch = data_batch.to(device)
 
             ind_select = []
@@ -64,15 +67,25 @@ def trainer(n_epochs, data_root, batch_size,
             out = lgcn(data_batch)
 
             y_to_score = out.y.index_select(0, ind_select)
+            loss = 0.0
+            for pred_type, ref_type in zip(y_to_score.permute(1, 0), data_batch.y.permute(1, 0)):
+                loss += F.mse_loss(pred_type, ref_type)
+            loss = loss / float(y_to_score.shape[1])
 
-            #loss
-            #loss.backward()
-            #optimizer.step()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        exp_lr_scheduler.step()
+
+        running_loss = running_loss / (k_batch + 1)
+        print ('Epoch {} with running loss (training) {}'.format(k_epoch, running_loss))
 
 
 if __name__ == '__main__':
     #rudimentary_test()
-    trainer(n_epochs=2, data_root='./data_processed_train', batch_size=1,
-            n_weight_versions=4, n_node_versions=4,
-            conv_type='GCNConv', conv_kwargs={},
-            lr_init=0.01, scheduler_step_size=7, scheduler_gamma=0.1)
+    trainer(n_epochs=20, data_root='./data_processed_train', batch_size=1,
+            n_weight_versions=1, n_node_versions=16,
+            conv_type='GMMConv', conv_n_iter=2, conv_kwargs={'dim':4, 'kernel_size':32},
+            lr_init=0.1, scheduler_step_size=10, scheduler_gamma=0.1)
--- model.py
+++ model.py
@@ -2,10 +2,13 @@
 
 '''
 import torch
-import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, GMMConv, ChebConv
+from torch.nn.utils import weight_norm
+from torch_geometric.nn import GCNConv, GMMConv, ChebConv, SGConv
 
 from torch_geometric.data import Data
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 class LatentGraph(torch.nn.Module):
     '''Bla bla for now
@@ -14,7 +17,8 @@ class LatentGraph(torch.nn.Module):
     def __init__(self, n_in_edge_props, n_in_node_props,
                  n_latent_edges, n_latent_nodes,
                  n_out_node_props,
-                 conv_type='GCNConv',
+                 edge_latent_epsilon=1e-5,
+                 conv_type='GCNConv', conv_n_iter=1,
                  conv_kwargs={}):
 
         super(LatentGraph, self).__init__()
@@ -25,39 +29,48 @@ class LatentGraph(torch.nn.Module):
         self.n_latent_nodes = n_latent_nodes
         self.n_out_node_props = n_out_node_props
 
-        self.conv_types = {'GCNConv' : GCNConv, 'GMMConv' : GMMConv, 'ChebConv' : ChebConv}
+        self.edge_latent_epsilon = edge_latent_epsilon
+        self.conv_types = {'GCNConv' : GCNConv, 'GMMConv' : GMMConv, 'ChebConv' : ChebConv, 'SGConv' : SGConv}
+        self.iter_graph_conv = conv_n_iter
         self.conv_kwargs = conv_kwargs
 
         try:
             self.graph_conv = self.conv_types[conv_type](in_channels=self.n_in_node_props,
                                                          out_channels=self.n_latent_nodes,
                                                          **self.conv_kwargs)
+            #print (count_parameters(self.graph_conv))
+            #raise RuntimeError
         except KeyError:
             raise RuntimeError('Unknown graph convolution type: {}'.format(conv_type))
 
+        # MLP at the end of the model
         self.fc_node_prop = torch.nn.Sequential(\
                             torch.nn.Linear(in_features=self.n_latent_edges * self.n_latent_nodes,
-                                            out_features=self.n_out_node_props, bias=True),
-                            torch.nn.ReLU())
-        self.fc_edge_latent = torch.nn.Sequential(\
-                              torch.nn.Linear(in_features=self.n_in_edge_props,
-                                              out_features=self.n_latent_edges, bias=True),
-                              torch.nn.ReLU())
+                                            out_features=self.n_latent_edges * self.n_latent_nodes, bias=True),
+                            torch.nn.ReLU(),
+                            torch.nn.Linear(in_features=self.n_latent_edges * self.n_latent_nodes,
+                                            out_features=self.n_out_node_props, bias=True))
+
+        # Linear transformation of edge properties to edge weights. Normalize weights so only relative
+        # differences in edge weights matters.
+        self.fc_edge_latent = weight_norm(torch.nn.Linear(in_features=self.n_in_edge_props,
+                                                          out_features=self.n_latent_edges, bias=False))
 
     def forward(self, data_graph):
 
         # Transform edge attributes to plurality of edge weights
         edge_props = self.fc_edge_latent(data_graph.edge_attr)
+        edge_props += self.edge_latent_epsilon * torch.ones(edge_props.shape)
 
         # Apply graph convolutions to plurality of differently weighted graphs
         collection_tensors = []
         for e_weight in edge_props.permute(1,0):
-            g_conv_out = self.graph_conv(data_graph.x, edge_index=data_graph.edge_index, edge_weight=e_weight)
+            g_conv_out = self.graph_conv(data_graph.x, edge_index=data_graph.edge_index, edge_attr=data_graph.edge_attr)
             collection_tensors.append(g_conv_out)
-        graph_step_0 = torch.cat(collection_tensors, 1)
+        graph_step_ = torch.cat(collection_tensors, 1)
 
         # Transform convoluted node data to plurality of node properties
-        graph_step_1 = self.fc_node_prop(graph_step_0)
+        graph_step_1 = self.fc_node_prop(graph_step_)
 
         return Data(y=graph_step_1, edge_index=data_graph.edge_index, edge_attr=data_graph.edge_attr)
 
