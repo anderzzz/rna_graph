8603c7b1aef31b9aa002ef6a4ed3a6e4c44bc535
anderzzz <ohrn.anders@gmail.com>
2020-09-24 12:58:11 +0200

    Bug fix to account for that the columns to be scored are not the first three, but a selection

--- loss.py
+++ loss.py
@@ -14,14 +14,15 @@ class RMSELoss(torch.nn.Module):
         return loss
 
 class MCRMSELoss(torch.nn.Module):
-    def __init__(self, num_scored=3):
+    def __init__(self, k_scored=(0,1,3)):
         super().__init__()
         self.rmse = RMSELoss()
-        self.num_scored = num_scored
+        self.k_scored = k_scored
+        self.num_scored = len(k_scored)
 
     def forward(self, yhat, y):
         score = 0
-        for i in range(self.num_scored):
+        for i in self.k_scored:
             score += self.rmse(yhat[:, i], y[:, i]) / self.num_scored
 
         return score
